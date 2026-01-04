971e861dfdd9e37d7cb9028b93a2fe9a43bda5fd
Thet Myat Moe <tzm295@student.bham.ac.uk>
2026-01-04 23:01:03 +0000

    Desktop&Mobile Navigation bar

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,11 @@
-import { SignedOut, SignInButton, SignUpButton } from "@clerk/nextjs";
+import ModeToggle from "@/components/ModeToggle";
+import { Button } from "@/components/ui/button";
+import { SignedIn, SignedOut, SignInButton, SignUpButton, UserButton } from "@clerk/nextjs";
 
 export default function Home() {
   return (
-    <div>
-      <SignedOut>
-              <SignInButton mode="modal" />
-              
-              <SignUpButton>
-                <button className="bg-[#6c47ff] text-ceramic-white rounded-full font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 cursor-pointer">
-                  Sign Up
-                </button>
-              </SignUpButton>
-        </SignedOut>
+    <div className="m-4">
+      Home Page Content
     </div>
 
   );

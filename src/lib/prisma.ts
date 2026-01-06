32ed288549c414d2b7e51813ab990915f0e878b8
Thet Myat Moe <tzm295@student.bham.ac.uk>
2026-01-06 23:10:04 +0000

    Downgraded prisma7 to prisma6

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,14 +1,14 @@
-// /src/lib/prisma.ts
+// src/lib/prisma.ts
 import { PrismaClient } from "@prisma/client";
 
-declare global {
-  var prisma: PrismaClient | undefined;
-}
+const globalForPrisma = globalThis as unknown as {
+  prisma?: PrismaClient;
+};
 
 export const prisma =
-  global.prisma ??
-  new PrismaClient({
-    log: ["query", "error"],
-  });
+  globalForPrisma.prisma ??
+  new PrismaClient();
 
-if (process.env.NODE_ENV !== "production") global.prisma = prisma;
+if (process.env.NODE_ENV !== "production") {
+  globalForPrisma.prisma = prisma;
+}

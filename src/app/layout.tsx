4ddd2862ce69484544f4a3c5b83b79b7be2be184
Thet Myat Moe <tzm295@student.bham.ac.uk>
2026-01-06 23:58:27 +0000

    Sidebar component for unauthenticated user

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,6 +4,7 @@ import "./globals.css";
 import { ClerkProvider } from "@clerk/nextjs";
 import { ThemeProvider } from "@/components/ThemeProvier";
 import NavBar from "@/components/NavBar";
+import Sidebar from "@/components/Sidebar";
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
@@ -47,7 +48,7 @@ export default function RootLayout({
                   <div className="grid grid-cols-1 lg:grid-cols-12 gap-6">
                     
                     <div className="hidden lg:block lg:col-span-3">
-                      sidebar
+                      <Sidebar/>
                     </div>
 
                     <div className="lg:col-span-9">
---
+++ src/components/Sidebar.tsx
@@ -0,0 +1,44 @@
+import { currentUser } from '@clerk/nextjs/server'
+import React from 'react'
+import { Card, CardContent, CardHeader, CardTitle } from "./ui/card";
+import { SignInButton, SignUpButton } from '@clerk/nextjs';
+import { Button } from './ui/button';
+
+async function Sidebar() {
+
+  const authUser = await currentUser();
+
+  if (!authUser) return <UnAuthenticatedSidebar/>
+
+
+  return (
+    <div>Sidebar</div>
+  )
+}
+
+export default Sidebar;
+
+const UnAuthenticatedSidebar = () => (
+  <div className="sticky top-20">
+    <Card>
+      <CardHeader>
+        <CardTitle className="text-center text-xl font-semibold">Welcome Back!</CardTitle>
+      </CardHeader>
+      <CardContent>
+        <p className="text-center text-muted-foreground mb-4">
+          Login to access your profile and connect with others.
+        </p>
+        <SignInButton mode="modal">
+          <Button className="w-full" variant="outline">
+            Login
+          </Button>
+        </SignInButton>
+        <SignUpButton mode="modal">
+          <Button className="w-full mt-2" variant="default">
+            Sign Up
+          </Button>
+        </SignUpButton>
+      </CardContent>
+    </Card>
+  </div>
+);
